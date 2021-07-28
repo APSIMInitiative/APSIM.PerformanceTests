00329468d999562c458f85f89a72cff859fdaa57
Dean Holzworth <Dean.Holzworth@csiro.au>
2021-07-28 14:03:09 +1000

    Added code to create the database if it doesn't exist

--- APSIM.POStats.Portal/Startup.cs
+++ APSIM.POStats.Portal/Startup.cs
@@ -39,6 +39,12 @@ namespace APSIM.POStats.Portal
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<StatsDbContext>();
+                context.Database.EnsureCreated();
+            }
+
             app.UseExceptionHandler("/Error");
             app.UseHsts();
             app.UseHttpsRedirection();
