60c038b8ad3455b3ea9c94786c8a55d97bb9cd1d
Andrew Paroz <par456@csiro.au>
2025-05-01 11:07:28 +1000

    Turn variables into properties

--- APSIM.POStats.Shared/GitHub/GitHubStatusDetails.cs
+++ APSIM.POStats.Shared/GitHub/GitHubStatusDetails.cs
@@ -4,16 +4,16 @@
     public class GitHubStatusDetails
     {
         /// <summary>State to set status to (success, pending, failure)</summary>
-        public string state;
+        public string state { get; set; }
 
         /// <summary>Url to link check to (the POStats page for that PR)</summary>
-        public string target_url;
+        public string target_url { get; set; }
 
         /// <summary>Description of status (Same, Different)</summary>
-        public string description;
+        public string description { get; set; }
 
         /// <summary>Name of Check (APSIM.POStats)</summary>
-        public string context;
+        public string context { get; set; }
 
         /// <summary>Constructor</summary>
         public GitHubStatusDetails(string state, string target_url, string description, string context)
