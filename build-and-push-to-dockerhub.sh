4879520f748d000b0316b06b4e970cd614deabe4
Julian Rich <123442863+ric394@users.noreply.github.com>
2025-09-11 14:55:23 +1000

    Add a script that builds and pushes postats2 images

---
+++ build-and-push-to-dockerhub.sh
@@ -0,0 +1,17 @@
+# This script builds and pushes the Docker images to Docker Hub
+
+echo "Building and pushing Docker images to Docker Hub..."
+echo "Make sure you are logged in to Docker Hub using 'docker login' before running this script."
+echo "Also ensure that you have permission to push to the 'apsiminitiative' repository."
+echo "You can login as apsimbot if you have the credentials."
+echo "You can run './build.sh' to build the images locally before pushing."
+echo ""
+
+# build the image first
+./build.sh
+
+# push the images to Docker Hub
+docker push apsiminitiative/postats2-portal:latest
+docker push apsiminitiative/postats2-collector:latest
+
+echo "Done."
