25bce254ea1599c83946105f69fb9282cf5ffa27
Andrew Paroz <par456@csiro.au>
2026-04-13 10:25:31 +1000

    Add in accepted variables even if not in current

--- APSIM.POStats.Shared/Comparison/TableComparison.cs
+++ APSIM.POStats.Shared/Comparison/TableComparison.cs
@@ -69,15 +69,15 @@ namespace APSIM.POStats.Shared.Comparison
                     matchingAcceptedVariables.Add(acceptedVariable);
                     variables.Add(new VariableComparison(currentVariable, acceptedVariable));
                 }
-
-                // Add in variables that are in the accepted table but not in the current table.
-                if (Accepted != null)
-                {
-                    var variablesNotInCurrent = Accepted.Variables.Except(matchingAcceptedVariables);
-                    foreach (var acceptedTable in variablesNotInCurrent)
-                        variables.Add(new VariableComparison(null, acceptedTable));
-                }
             }
+            // Add in variables that are in the accepted table but not in the current table.
+            if (Accepted != null)
+            {
+                var variablesNotInCurrent = Accepted.Variables.Except(matchingAcceptedVariables);
+                foreach (var acceptedTable in variablesNotInCurrent)
+                    variables.Add(new VariableComparison(null, acceptedTable));
+            }
+
             return variables.OrderBy(v => v.Name).ToList();
         }
     }
